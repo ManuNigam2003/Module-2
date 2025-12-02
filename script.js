ee08d309dcb2dac2f3246bec432a45f71b80a79e
Manu Nigam <92291248+ManuNigam2003@users.noreply.github.com>
2025-12-02 13:21:38 +0530

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-/* Iteration 4: Make the Everything Work */
-const buttonBackground = document.getElementById("button-background");
-const imgBackground = document.getElementById("background");
+// Make the application functional by adding javascript functionalities.
+const buttonBackground = document.getElementById('button-background');
+const imgBackground = document.getElementById('background');
 
-buttonBackground.oninput = ()=>{
+buttonBackground.oninput = () => {
   imgBackground.style.fill = buttonBackground.value;
-}
+};
+const buttonfaceplate = document.getElementById('button-face-plate');
+const faceplate = document.getElementById('face-plate');
 
-const buttonfaceplate = document.getElementById("button-face-plate");
-const faceplate = document.getElementById("face-plate");
-
-buttonfaceplate.oninput = ()=>{
+buttonfaceplate.oninput = () => {
   faceplate.style.fill = buttonfaceplate.value;
-}
+};
 
-const buttoneyes = document.getElementById("button-eyes");
-const eyes = document.getElementById("eyes");
+const buttoneyes = document.getElementById('button-eyes');
+const eyes = document.getElementById('eyes');
 
-buttoneyes.oninput = ()=>{
+buttoneyes.oninput = () => {
   eyes.style.fill = buttoneyes.value;
-}
+};
 
-const buttonhelmet = document.getElementById("button-helmet");
-const helmet = document.getElementById("helmet");
+const buttonhelmet = document.getElementById('button-helmet');
+const helmet = document.getElementById('helmet');
 
-buttonhelmet.oninput = ()=>{
+buttonhelmet.oninput = () => {
   helmet.style.fill = buttonhelmet.value;
-}
+};
